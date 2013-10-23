adb3f3ef7123a5723b6e5904233a07f08289217d
Zach Obront <zobront@gmail.com>
2013-10-23 17:17:25 -0300

    Restarting with Albertos

---
+++ README.md
@@ -0,0 +1,8 @@
+# One month rails 
+
+The pinteresting sample application for
+[*One month rails*][http://onemonthrails.com]
+
+by [Alberto Brandão][http://alberto.io]
+
+2013 exosphere
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '2789a433fe7f693eff4bbe2e76df0cfd6db1e8c1dc4cf946b7d67e8da6d1d7c5876622701112c45923f29a876a27854278d11dd56dfbcf8fc5933a217b43fb30'
